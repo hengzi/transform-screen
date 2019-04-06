05df049ad50029c3f55772c9e3b782557cc0ce9f
hengzi <cn.zhenhua@qq.com>
2019-04-06 18:50:22 +0800

    build to docs

---
+++ docs/precache-manifest.f283cf9d1ee4c9c3282271701cbc4287.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48346e4ffe791e1dab0f",
+    "url": "/transform-screen/static/js/runtime~main.70455cb4.js"
+  },
+  {
+    "revision": "11b5099c6e30bd74c5ae",
+    "url": "/transform-screen/static/js/main.72d2152d.chunk.js"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/transform-screen/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "11b5099c6e30bd74c5ae",
+    "url": "/transform-screen/static/css/main.a85fbb96.chunk.css"
+  },
+  {
+    "revision": "20714a51c0672b3c527dac5f88359edc",
+    "url": "/transform-screen/index.html"
+  }
+];
